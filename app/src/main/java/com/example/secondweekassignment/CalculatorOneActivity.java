51dcea13f1c94407765c4d3a9b5bdda4c1ecb710
Suman Bhujel <xnamus@outlook.com>
2019-10-14 13:28:44 +0545

    switch case commit

--- app/src/main/java/com/example/secondweekassignment/CalculatorOneActivity.java
+++ app/src/main/java/com/example/secondweekassignment/CalculatorOneActivity.java
@@ -12,6 +12,10 @@ import android.widget.TextView;
 public class CalculatorOneActivity extends AppCompatActivity implements View.OnClickListener{
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSubtract,btnMultiply,btnDivide;
     EditText editText;
+    String displayNum = "";
+    String op;
+    int num1,num2,res;
+
 
 
 
@@ -30,7 +34,7 @@ public class CalculatorOneActivity extends AppCompatActivity implements View.OnC
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
         btn0 = findViewById(R.id.btn0);
-        editText = findViewById(R.id.Result);
+        editText = findViewById(R.id.result);
         btnAdd = findViewById(R.id.btnPlus);
         btnSubtract = findViewById(R.id.btnMinus);
         btnMultiply = findViewById(R.id.btnMultiply);
@@ -43,13 +47,50 @@ public class CalculatorOneActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-
+        switch (view.getId())
+        {
             case R.id.btn1:
+                displayNum += 1;
+                editText.setText(displayNum);
+                break;
 
+            case R.id.btn2:
+                displayNum += 2;
+                editText.setText(displayNum);
+                break;
 
+            case R.id.btnPlus:
+                num1 = Integer.parseInt(displayNum);
+                displayNum = "";
+                op = "+";
+                break;
 
+            case R.id.btnEqual:
+                num2 = Integer.parseInt(displayNum);
+                res = getResult(op,num1,num2);
+                editText.setText(String.valueOf(res));
+                break;
+        }}
+        private int getResult(String op,int n1, int n2)
+        {
+            switch(op)
+            {
+                case "+":
+                    return n1+n2;
+
+                case "-":
+                    return n1-n2;
+
+                case "*":
+                    return n1*n2;
+
+                case "/":
+                    return n1/n2;
+
+                default:
+                    return 0;
+            }
         }
 
     }
-}
+
