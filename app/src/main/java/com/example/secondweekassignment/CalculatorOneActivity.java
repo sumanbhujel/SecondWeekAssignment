5d336a99477579d9e14fa9739924368e24430973
Suman Bhujel <xnamus@outlook.com>
2019-10-20 10:10:35 +0545

    calculator editing

--- app/src/main/java/com/example/secondweekassignment/CalculatorOneActivity.java
+++ app/src/main/java/com/example/secondweekassignment/CalculatorOneActivity.java
@@ -8,12 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CalculatorOneActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSubtract,btnMultiply,btnDivide;
+public class CalculatorOneActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnClear, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnEqual, btnAdd, btnSubtract, btnMultiply, btnDivide;
     EditText editText;
     String displayNum = "";
     String op;
-    int num1,num2,res;
+    int num1, num2, res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +31,33 @@ public class CalculatorOneActivity extends AppCompatActivity implements View.OnC
         btn9 = findViewById(R.id.btn9);
         btn0 = findViewById(R.id.btn0);
         editText = findViewById(R.id.result);
+        btnEqual = findViewById(R.id.btnEqual);
         btnAdd = findViewById(R.id.btnPlus);
         btnSubtract = findViewById(R.id.btnMinus);
         btnMultiply = findViewById(R.id.btnMultiply);
         btnDivide = findViewById(R.id.btnDivide);
+        btnClear = findViewById(R.id.btnClear);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnEqual.setOnClickListener(this);
         btnAdd.setOnClickListener(this);
         btnDivide.setOnClickListener(this);
         btnMultiply.setOnClickListener(this);
         btnSubtract.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.btn1:
                 displayNum += 1;
                 editText.setText(displayNum);
@@ -55,38 +68,102 @@ public class CalculatorOneActivity extends AppCompatActivity implements View.OnC
                 editText.setText(displayNum);
                 break;
 
+            case R.id.btn3:
+                displayNum += 3;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn4:
+                displayNum += 4;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn5:
+                displayNum += 5;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn6:
+                displayNum += 6;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn7:
+                displayNum += 7;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn8:
+                displayNum += 8;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn9:
+                displayNum += 9;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btn0:
+                displayNum += 0;
+                editText.setText(displayNum);
+                break;
+
+            case R.id.btnClear:
+                displayNum = "";
+                editText.setText(displayNum);
+                break;
+
             case R.id.btnPlus:
                 num1 = Integer.parseInt(displayNum);
                 displayNum = "";
                 op = "+";
                 break;
 
+            case R.id.btnMinus:
+                num1 = Integer.parseInt(displayNum);
+                displayNum = "";
+                op = "-";
+                break;
+
+            case R.id.btnMultiply:
+                num1 = Integer.parseInt(displayNum);
+                displayNum = "";
+                op = "*";
+                break;
+
+            case R.id.btnDivide:
+                num1 = Integer.parseInt(displayNum);
+                displayNum = "";
+                op = "/";
+                break;
+
             case R.id.btnEqual:
                 num2 = Integer.parseInt(displayNum);
-                res = getResult(op,num1,num2);
+                res = getResult(op, num1, num2);
                 editText.setText(String.valueOf(res));
+                displayNum = "";
                 break;
-        }}
-        private int getResult(String op,int n1, int n2)
-        {
-            switch(op)
-            {
-                case "+":
-                    return n1+n2;
-
-                case "-":
-                    return n1-n2;
-
-                case "*":
-                    return n1*n2;
-
-                case "/":
-                    return n1/n2;
-
-                default:
-                    return 0;
-            }
         }
-
     }
+
+    private int getResult(String op, int n1, int n2) {
+        switch (op) {
+            case "+":
+                return n1 + n2;
+
+            case "-":
+                return n1 - n2;
+
+            case "*":
+                return n1 * n2;
+
+            case "/":
+                return n1 / n2;
+
+            default:
+                return 0;
+        }
+    }
+
+}
 
