e30520b7acc51423fb73e28f8d0c218fe5537938
Suman Bhujel <xnamus@outlook.com>
2019-12-02 09:21:33 +0545

    implicit intent

---
+++ app/src/main/java/com/example/secondweekassignment/ImplicitIntentActivity.java
@@ -0,0 +1,14 @@
+package com.example.secondweekassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImplicitIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_intent);
+    }
+}
--- app/src/main/java/com/example/secondweekassignment/SharedPreActivity.java
+++ app/src/main/java/com/example/secondweekassignment/SharedPreActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SharedPreActivity extends AppCompatActivity {
 
@@ -16,12 +17,27 @@ public class SharedPreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shared_pre);
 
         //getcontext for current activity  and variable is saved in mysp
-        sp = getApplicationContext().getSharedPreferences("mysp", MODE_PRIVATE);
-        editor = sp.edit();
+       sp = getApplicationContext().getSharedPreferences("mysp", MODE_PRIVATE);
+
+       /* editor = sp.edit();
         editor.putString("name", "Suman");
         editor.putInt("age", 30);
         editor.putLong("phone",982317160);
         editor.putBoolean("isMember", true);
-        editor.commit(); //commit garnu parcha
+        editor.commit(); //commit garnu parcha*/
+
+
+        String n =  sp.getString("name","");
+        int a = sp.getInt("age", 0);
+        Long p = sp.getLong("phone" , 0);
+        boolean im = sp.getBoolean("isMember", false);
+        int b = sp.getInt("zip",0);
+
+        Toast.makeText(this, "Name:"+n +"\n"+
+                "Age: "+ a+"\n"+
+                "Phone: "+ p +"\n"+
+                "Is Member: " +im+"\n"+
+                "Zip: "+ b, Toast.LENGTH_SHORT).show();
+
     }
 }
