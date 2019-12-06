0b9e819e4de0101260b39cd8a22c0a8ca1cf29a5
Suman Bhujel <xnamus@outlook.com>
2019-12-06 10:54:31 +0545

    student model and dynamic insert data, get data

--- app/src/main/java/com/example/secondweekassignment/DatabaseActivity.java
+++ app/src/main/java/com/example/secondweekassignment/DatabaseActivity.java
@@ -3,12 +3,23 @@ package com.example.secondweekassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+
 import com.example.secondweekassignment.database.DbHelper;
+import com.example.secondweekassignment.model.Student;
+
+import java.util.List;
 
 public class DatabaseActivity extends AppCompatActivity {
 
+    EditText editTextN, editTextE, editTextP;
+    Button buttonAdd;
+    DbHelper dbHelper;
 
 
     @Override
@@ -16,11 +27,39 @@ public class DatabaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
 
-        DbHelper dbHelper = new DbHelper(this);
-        dbHelper.addStudent();
+        editTextN = findViewById(R.id.edName);
+        editTextE = findViewById(R.id.edEmail);
+        editTextP = findViewById(R.id.edPhone);
+        buttonAdd = findViewById(R.id.btnAdd);
 
+        dbHelper = new DbHelper(this);
 
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String n = editTextN.getText().toString();
+                String e = editTextE.getText().toString();
+                String p = editTextP.getText().toString();
 
+                Student student = new Student(0, n, e, p);
+
+                if (dbHelper.addStudent(student)) {
+                    Toast.makeText(DatabaseActivity.this, "Successfully Added", Toast.LENGTH_SHORT).show();
+                    editTextN.setText("");
+                    editTextE.setText("");
+                    editTextP.setText("");
+                }
+            }
+        });
+
+        List<Student> students = dbHelper.getStudents();
+        for(Student student:students){
+            Log.d("ID : ",String.valueOf(student.getId()));
+            Log.d("Name: ",student.getName());
+            Log.d("Email: ",student.getEmail());
+            Log.d("Phone: ",student.getPhone());
+
+        }
 
 
     }
--- app/src/main/java/com/example/secondweekassignment/database/DbHelper.java
+++ app/src/main/java/com/example/secondweekassignment/database/DbHelper.java
@@ -1,11 +1,19 @@
 package com.example.secondweekassignment.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import com.example.secondweekassignment.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.secondweekassignment.model.Student.TBL_CREATE;
 
 public class DbHelper extends SQLiteOpenHelper {
 
@@ -13,7 +21,6 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "mydb";
     private static final int DB_VERSION = 1;
 
-    String tbl_create = "CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT,phone TEXT)";
 
     public DbHelper(@Nullable Context context) {
 
@@ -22,7 +29,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(tbl_create);
+        sqLiteDatabase.execSQL(TBL_CREATE);
 
 
     }
@@ -32,15 +39,33 @@ public class DbHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean addStudent(){
+    public boolean addStudent(Student student) {
         try {
             SQLiteDatabase db = getWritableDatabase();
-            db.execSQL("INSERT INTO students (name, email, phone) VALUES ('Suman','suman@gmail.com','9823171680')");
+            db.execSQL("INSERT INTO students (name, email, phone) VALUES ('" + student.getName() + "','" + student.getEmail() + "','" + student.getPhone() + "')");
             return true;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             Log.d("DBEx", e.toString());
             return false;
         }
+    }
+
+    public List<Student> getStudents() {
+        List<Student> studentList = new ArrayList<>();
+        try {
+            SQLiteDatabase db = getReadableDatabase();
+            Cursor cursor = db.rawQuery("SELECT * FROM students", null);
+
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    studentList.add(new Student(cursor.getInt(0), cursor.getString(1),
+                            cursor.getString(2), cursor.getString(3)));
+                }
+            }
+
+        } catch (Exception e) {
+            Log.d("DBEx", e.toString());
+        }
+        return studentList;
     }
 }
